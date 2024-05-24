dec2054d3dd7540ff46484a8b0796ae8c9a4f647
Aditya Bhattad <adityabhattad18@gmail.com>
2024-05-24 13:26:05 +0530

    fix: deploy funding vault

--- src/FairFund.sol
+++ src/FairFund.sol
@@ -2,7 +2,9 @@
 pragma solidity ^0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {FundingVault} from "./FundingVault.sol";
+import {VotingPowerToken} from "./VotingPowerToken.sol";
 
 contract FairFund is Ownable {
     // Errors //
@@ -10,6 +12,9 @@ contract FairFund is Ownable {
     error FairFund__TallyDateCannotBeInThePast();
     error FairFund__MaxRequestableAmountCannotBeLessThanMinRequestableAmount();
     error FairFund__MinRequestableAmountCannotBeGreaterThanMaxRequestableAmount();
+
+    // State Variables //
+    uint256 private s_fundingVaultIdCounter;
 
     // Events //
     event FundingVaultDeployed(address fundingVault);
@@ -33,9 +38,22 @@ contract FairFund is Ownable {
         if (_minRequestableAmount > _maxRequestableAmount) {
             revert FairFund__MinRequestableAmountCannotBeGreaterThanMaxRequestableAmount();
         }
+        s_fundingVaultIdCounter++;
+        uint256 fundingVaultId = s_fundingVaultIdCounter;
+        string memory fundingVaultIdString = Strings.toString(fundingVaultId);
+        string memory votingPowerTokenName = string.concat("Voting Power Token ", fundingVaultIdString);
+        string memory votingPowerTokenSymbol = string.concat("VOTE_", fundingVaultIdString);
+        VotingPowerToken votingPowerToken = new VotingPowerToken(votingPowerTokenName, votingPowerTokenSymbol);
         FundingVault fundingVault = new FundingVault(
-            _fundingToken, _votingToken, _minRequestableAmount, _maxRequestableAmount, _tallyDate, _owner
+            _fundingToken,
+            _votingToken,
+            address(votingPowerToken),
+            _minRequestableAmount,
+            _maxRequestableAmount,
+            _tallyDate,
+            _owner
         );
+        votingPowerToken.transferOwnership(address(fundingVault));
         emit FundingVaultDeployed(address(fundingVault));
     }
 }
--- src/VotingPowerToken.sol
+++ src/VotingPowerToken.sol
@@ -2,26 +2,12 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VotingPowerToken is ERC20 {
-    // Errors //
-    error VotingPowerToken__CannotBeAZeroAddress();
-    error VotingPowerToken__NotAuthorized();
+contract VotingPowerToken is ERC20, Ownable {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(msg.sender) {}
 
-    // State Variables //
-    address private immutable i_fundingVault;
-
-    constructor(string memory _name, string memory _symbol, address _fundingVault) ERC20(_name, _symbol) {
-        if (_fundingVault == address(0)) {
-            revert VotingPowerToken__CannotBeAZeroAddress();
-        }
-        i_fundingVault = _fundingVault;
-    }
-
-    function mint(address _to, uint256 _amount) external {
-        if (msg.sender != i_fundingVault) {
-            revert VotingPowerToken__NotAuthorized();
-        }
+    function mint(address _to, uint256 _amount) external onlyOwner {
         _mint(_to, _amount);
     }
 }
