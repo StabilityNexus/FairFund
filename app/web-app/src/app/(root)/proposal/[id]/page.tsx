047fabe9d58422e041bc077011a49cc7c8d97509
Aditya Bhattad <adityabhattad18@gmail.com>
2024-06-17 17:18:44 +0530

    feat: implement proposal info page.(#9)

--- app/web-app/src/app/(root)/proposal/[id]/page.tsx
+++ app/web-app/src/app/(root)/proposal/[id]/page.tsx
@@ -1,9 +1,40 @@
-export default function ProposalDetailsPage(){
+import CardWrapper from "@/components/proposal-details-card-wrapper";
+import { Separator } from "@/components/ui/separator";
+import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
+
+
+
+export default async function ProposalDetailsPage({
+    params
+}:{
+    params:{
+        id:string
+    }
+}){
+    const id = params.id;
+    const proposal = await prisma.proposal.findUnique({
+        where:{
+            id:Number(id)
+        }
+    })
+    if(!proposal){
+        redirect('/dashboard')
+    }
     return (
-        <div>
-            <h1>
-                Proposal Details.
-            </h1>
+        <div className="p-8 w-full h-full flex justify-center items-center flex-col gap-4">
+            <div className="space-y-2 w-full">
+                <h3 className="text-lg">
+                    Proposal Details
+                </h3>
+                <p className="text-sm text-muted-foreground">
+                    View the details of the proposal.
+                </p>
+            </div>
+            <Separator className="bg-primary/10" />
+            <CardWrapper
+                proposal={proposal}
+            />
         </div>
     )
 }   
---
+++ app/web-app/src/components/proposal-details-card-wrapper.tsx
@@ -0,0 +1,84 @@
+import { type Proposal } from "@prisma/client";
+import { Coins, File, Fingerprint, User2 } from "lucide-react";
+import { Card } from "@/components/ui/card";
+import CardBody from "@/components/card-body";
+import { Badge } from "@/components/ui/badge";
+
+const iconMap = {
+    id: Fingerprint,
+    money: Coins,
+    creator: User2,
+    recipient: User2,
+    description: File
+}
+
+interface ProposalDetailsCardWrapperProps {
+    proposal: Proposal;
+}
+
+
+export default function ProposalDetailsCardWrapper({
+    proposal
+}: ProposalDetailsCardWrapperProps) {
+    return (
+        <>
+            <div className="m-2 w-full flex justify-center items-center">
+                <CardBody
+                    className="w-full"
+                    title="Description"
+                    icon={iconMap["description"]}
+                    body={proposal.description}
+                    bodyClassName="text-sm font-light"
+                />
+            </div>
+            <div className="m-2 w-full grid gap-6 md:grid-cols-2">
+                <CardBody
+                    className="max-h-[100px] overflow-hidden"
+                    bodyClassName="text-md text-muted-foreground font-normal w-full"
+                    title="Creator" 
+                    icon={iconMap["creator"]}
+                    body={
+                        <div className="flex flex-row items-center justify-start">
+                            <p className="mr-2 text-secondary-foreground text-xs">
+                                Wallet Address:
+                            </p>
+                            <Badge variant="outline" className="px-4 py-2 w-full truncate">
+                                {proposal!.proposerAddress}
+                            </Badge>
+                        </div>
+                    }
+                />
+                <CardBody
+                    className="max-h-[100px] overflow-hidden"
+                    bodyClassName="text-md text-muted-foreground font-normal w-full"
+                    title="Recipient" 
+                    icon={iconMap["recipient"]}
+                    body={
+                        <div className="flex flex-row items-center justify-start">
+                            <p className="mr-2 text-secondary-foreground text-xs">
+                                Wallet Address:
+                            </p>
+                            <Badge variant="outline" className="px-4 py-2 w-full truncate">
+                                {proposal!.recipient}
+                            </Badge>
+                        </div>
+                    }
+                />
+                <CardBody
+                    className="max-h-[100px] overflow-hidden"
+                    bodyClassName="text-md text-muted-foreground font-normal w-full"
+                    title="Min Request Amount" 
+                    icon={iconMap["money"]}
+                    body={proposal!.minRequestAmount}
+                />
+                <CardBody
+                    className="max-h-[100px] overflow-hidden"
+                    bodyClassName="text-md text-muted-foreground font-normal w-full"
+                    title="Max Request Amount" 
+                    icon={iconMap["money"]}
+                    body={proposal!.maxRequestAmount}
+                />
+            </div>
+        </>
+    )
+}
