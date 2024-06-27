c1c781c5749cfa85fc1e63f7f9e61825832994c1
Aditya Bhattad <adityabhattad18@gmail.com>
2024-06-27 11:41:17 +0530

    feat: add withdraw voting token button on vault details page.

--- app/web-app/src/app/(root)/vault/[id]/page.tsx
+++ app/web-app/src/app/(root)/vault/[id]/page.tsx
@@ -1,12 +1,14 @@
-import TableWrapper from '@/components/proposal-table/table-wrapper';
-import { Button } from '@/components/ui/button';
-import CardWrapper from '@/components/vault-details-card-wrapper';
+
 import Link from 'next/link';
 import prisma from '@/lib/db';
 import { redirect } from 'next/navigation';
-import DistributeFundsButton from '@/components/distrubute-funds-botton';
+import { fundingVaultABI } from '@/blockchain/constants';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
+import TableWrapper from '@/components/proposal-table/table-wrapper';
+import { Button } from '@/components/ui/button';
+import CardWrapper from '@/components/vault-details-card-wrapper';
+import { BlockchainActionButton } from '@/components/blockchain-action-button';
 import { ArrowRight, PlusCircle, Coins, UserPlus } from 'lucide-react';
 
 const ActionButton = ({ href, icon, text, disabled }: {
@@ -51,7 +53,7 @@ export default async function VaultDetailsPage({
     if (!vault) {
         redirect('/dashboard');
     }
-    const isVaultClosed = vault.tallyDate.getTime() < Date.now();
+    const isTallyDatePassed = vault.tallyDate.getTime() < Date.now();
 
     return (
         <div className="container mx-auto p-6 space-y-8">
@@ -77,28 +79,44 @@ export default async function VaultDetailsPage({
                     <CardTitle className="text-2xl font-bold">Actions</CardTitle>
                 </CardHeader>
                 <CardContent>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <ActionButton
                             href={`/proposal/new?vaultId=${id}`}
                             icon={<PlusCircle className="mr-2 h-4 w-4" />}
                             text="Create Proposal"
-                            disabled={isVaultClosed}
+                            disabled={isTallyDatePassed}
                         />
                         <ActionButton
                             href={`/vault/deposit?vaultId=${id}`}
                             icon={<Coins className="mr-2 h-4 w-4" />}
                             text="Deposit Funding Tokens"
-                            disabled={isVaultClosed}
+                            disabled={isTallyDatePassed}
                         />
                         <ActionButton
                             href={`/vault/register?vaultId=${id}`}
                             icon={<UserPlus className="mr-2 h-4 w-4" />}
                             text="Register to Vote"
-                            disabled={isVaultClosed}
+                            disabled={isTallyDatePassed}
                         />
-                        <DistributeFundsButton
-                            fundingVault={vault}
+                        <BlockchainActionButton
+                            smartContractAddress={vault.vaultAddress as `0x${string}`}
+                            functionName='distributeFunds'
+                            smartContractABI={fundingVaultABI}
+                            buttonText='Distrubute Funds'
+                            iconName='distrubuteFunds'
+                            isDisabled={!isTallyDatePassed}
+                            successMessage='Funds distributed successfully.'
                             className="w-full h-full flex items-center justify-center px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90 rounded-md"
+                        />
+                        <BlockchainActionButton
+                            smartContractAddress={vault.vaultAddress as `0x${string}`}
+                            functionName='releaseVotingTokens'
+                            smartContractABI={fundingVaultABI}
+                            buttonText='Withdraw Voting Tokens'
+                            iconName='creditCard'
+                            isDisabled={!isTallyDatePassed}
+                            successMessage='Voting tokens withdrawn successfully.'
+                            className='w-full h-full flex items-center justify-center px-4 py-2 bg-primary text-primary-foreground hover:bg-primary/90 rounded-md'
                         />
                     </div>
                 </CardContent>
