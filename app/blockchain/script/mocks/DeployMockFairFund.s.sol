8e6204eb75f8e441e69c4f0405f73088e0c04fbb
Aditya Bhattad <adityabhattad18@gmail.com>
2024-06-29 20:53:08 +0530

    chore: add makefile for smart contracts/

--- app/blockchain/script/mocks/DeployMockFairFund.s.sol
+++ app/blockchain/script/mocks/DeployMockFairFund.s.sol
@@ -5,7 +5,7 @@ import {Script} from "forge-std/Script.sol";
 import {MockFairFund} from "../../src/mocks/MockFairFund.sol";
 import {HelperConfig} from "../HelperConfig.s.sol";
 
-contract DeployMockERC20 is Script {
+contract DeployMockFairFund is Script {
     function run() external returns (MockFairFund mockFairFund, HelperConfig helperConfig) {
         helperConfig = new HelperConfig();
         (uint256 deployerKey) = helperConfig.activeNetworkConfig();
@@ -13,7 +13,7 @@ contract DeployMockERC20 is Script {
         vm.startBroadcast(deployerKey);
         mockFairFund = new MockFairFund();
         vm.stopBroadcast();
-        string memory deploymentInfo = string.concat('{"mockERC20":"', vm.toString(address(mockFairFund)), '"}');
+        string memory deploymentInfo = string.concat('{"mockFairFund":"', vm.toString(address(mockFairFund)), '"}');
         vm.writeFile("constants/anvil/fairFund_deployment.json", deploymentInfo);
     }
 }
