7e58aa31b2adc0f5e5f8c11c581d0a1c4af29408
Aditya Bhattad <adityabhattad18@gmail.com>
2024-07-24 01:03:32 +0530

    feat: add tabbed layout for space-form.

--- app/web-app/src/components/space-form.tsx
+++ app/web-app/src/components/space-form.tsx
@@ -1,4 +1,15 @@
 import { Button } from '@/components/ui/button';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
 
 interface SpaceFormInterface {
     nextComp: () => void;
@@ -6,8 +17,44 @@ interface SpaceFormInterface {
 
 export default function SpaceForm({ nextComp }: SpaceFormInterface) {
     return (
-        <div className="flex flex-col w-full justify-center items-center">
-            <h1>Create or Select Existing Space (WIP🏗️)</h1>
+        <div className="w-full mx-auto">
+            <div className="my-6">
+                <h3 className="text-2xl font-semibold mb-2 ">
+                    Create or Select a space
+                </h3>
+                <p className="text-sm text-muted-foreground">
+                    Start by either creating a new space or selecting an
+                    existing one. This space will serve as a container for
+                    multiple vaults.
+                </p>
+            </div>
+            <Tabs defaultValue="create" className="w-full">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="create">Create New Space</TabsTrigger>
+                    <TabsTrigger value="select">
+                        Select Existing Space
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="create">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Space Details</CardTitle>
+                            <CardDescription>
+                                Enter name for your space and a clear and
+                                consice description.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-40">
+                            <div className="space-y-2">
+                                <div>Name</div>
+                                <div>Desc</div>
+                            </div>
+                            <Button>Submit</Button>
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="select">Select a space</TabsContent>
+            </Tabs>
             <Button type="button" onClick={nextComp}>
                 Next
             </Button>
