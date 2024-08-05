2edd07e96c03520e7dde163e493f2aa9a0db6858
Aditya Bhattad <adityabhattad18@gmail.com>
2024-08-06 00:56:49 +0530

    fix: signout.

--- app/web-app/src/wagmi/siwe/index.ts
+++ app/web-app/src/wagmi/siwe/index.ts
@@ -1,8 +1,6 @@
 'use client';
-
-import { signOut } from '@/actions/sign-out';
-import { createSIWEConfig, type SIWEConfig } from '@web3modal/siwe';
-import { getCsrfToken, getSession, signIn } from 'next-auth/react';
+import { createSIWEConfig } from '@web3modal/siwe';
+import { getCsrfToken, getSession, signIn, signOut } from 'next-auth/react';
 import { SiweMessage } from 'siwe';
 
 export const siweConfig = createSIWEConfig({
